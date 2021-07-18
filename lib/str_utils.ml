b421fcccaa3e7992ad0df436472a52803e011921
= <goh00@protonmail.com>
2021-07-18 20:47:40 +0000

    Remove unused Str_utils functions

--- lib/str_utils.ml
+++ lib/str_utils.ml
@@ -1,15 +1,5 @@
 open Core
 
-let char_at_index string index =
-  if Int.is_negative index then
-    String.sub string ~pos: ((String.length string) - index) ~len: 1
-  else
-    String.sub string ~pos: index ~len: 1
-
-let cut_char_at string index =
-  String.sub string ~pos: 0 ~len: index ^
-  String.sub string ~pos: (index + 1) ~len: ((String.length string) - (index + 1))
-
 let matches string regex = Str.string_match (Str.regexp regex) string 0
 
 let get_first_char string = String.prefix string 1
