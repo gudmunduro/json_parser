31e69dadb0030f2bddd567a7edd79e5014edd777
= <goh00@protonmail.com>
2021-08-07 01:46:05 +0000

    Add support for formatting json

--- bin/json_parser.ml
+++ bin/json_parser.ml
@@ -5,6 +5,6 @@ open Json_parser_lib.Decoder
 let () =
   let text = In_channel.read_all "json_test_file.json" in
   let parsed = decode_json text in
-  let encoded = encode_json parsed in
+  let encoded = encode_json parsed ~formatted: true in
   let () = print_endline "Parsed value:" in
   print_endline encoded
--- lib/encoder.ml
+++ lib/encoder.ml
@@ -1,34 +1,70 @@
 open Core
 open Json_types
 
-let rec encode_json json = 
-  let add_quotation string = ("\"" ^ string ^ "\"") in
-
-  let json_array_to_string array = (
-    let items_str = 
-      List.map array ~f: encode_json |>
-      String.concat ~sep: "," in
-    "[" ^ items_str ^ "]"
-  ) in
-
-  let json_object_to_string obj = (
-    let properties_str = 
-      Map.to_alist obj |>
-      List.map ~f: (fun (key, value) -> (add_quotation key) ^ ":" ^ (encode_json value)) |>
-      String.concat ~sep: "," in
-    "{" ^ properties_str ^ "}"
-  ) in
-
+let encode_json ?formatted: (formatted=false) json =
+  (* Shared *)
   let bool_to_string bool = (
     match bool with
     | true -> "true"
     | false -> "false"
   ) in
 
-  match json with
-  | String(value) -> value |> add_quotation
-  | Number(value) -> Float.to_string value
-  | Null -> "null"
-  | Array(array) -> json_array_to_string array
-  | Object(obj) -> json_object_to_string obj
-  | Bool(value) -> bool_to_string value
+  let add_quotation string = ("\"" ^ string ^ "\"") in
+
+  (* Minified *)
+  let rec encode_value json = (
+    let json_array_to_string array = (
+      let items_str = 
+        List.map array ~f: encode_value |>
+        String.concat ~sep: "," in
+      "[" ^ items_str ^ "]"
+    ) in
+
+    let json_object_to_string obj = (
+      let properties_str = 
+        Map.to_alist obj |>
+        List.map ~f: (fun (key, value) -> (add_quotation key) ^ ":" ^ (encode_value value)) |>
+        String.concat ~sep: "," in
+      "{" ^ properties_str ^ "}"
+    ) in
+
+    match json with
+    | String(value) -> value |> add_quotation
+    | Number(value) -> Float.to_string value
+    | Null -> "null"
+    | Array(array) -> json_array_to_string array
+    | Object(obj) -> json_object_to_string obj
+    | Bool(value) -> bool_to_string value
+  ) in
+    
+    (* Formatted *)
+    let rec encode_formatted_value json ~level = (
+      let indent = String.make (level * 4) ' ' in
+      let inner_indent = String.make ((level + 1) * 4) ' ' in
+
+      let json_array_to_string array = (
+        let items_str = 
+          List.map array ~f: (fun element -> (encode_formatted_value element ~level: (level + 1)) ) |>
+          String.concat ~sep: (",\n" ^ inner_indent)  in
+          "[" ^ "\n" ^ inner_indent ^ items_str ^ "\n" ^ indent ^ "]"
+      ) in
+    
+      let json_object_to_string obj = (
+        let properties_str = 
+          Map.to_alist obj |>
+          List.map ~f: (fun (key, value) -> (add_quotation key) ^ ": " ^ (encode_formatted_value value ~level: (level + 1))) |>
+          String.concat ~sep: (",\n" ^ inner_indent) in
+          "{" ^ "\n" ^ inner_indent ^ properties_str ^ "\n" ^ indent ^ "}"
+      ) in
+    
+      match json with
+      | String(value) -> value |> add_quotation
+      | Number(value) -> Float.to_string value
+      | Null -> "null"
+      | Array(array) -> json_array_to_string array
+      | Object(obj) -> json_object_to_string obj
+      | Bool(value) -> bool_to_string value
+    ) in
+
+    if formatted then encode_formatted_value json ~level: 0
+    else encode_value json
